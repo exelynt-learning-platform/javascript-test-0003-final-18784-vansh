0ca7675afe9374304c0cac4cdf3076589c4a290e
Vansh Gosavi <129474804+VanshGosavi07@users.noreply.github.com>
2026-03-14 14:41:02 +0530

    Refactor answer function to use fixed value 6

--- Test 3.js
+++ Test 3.js
@@ -1,12 +1,9 @@
-function answer(a = 6) {
-  for (let b = 1; b <= a; b++) {
-    let c = '';
-    let d = b % 2 === 1 ? 1 : 0;
-    
-    for (let e = 0; e < b; e++) {
-      c += ((d + e) % 2) + (e < b - 1 ? ' ' : '');
-    }
-    console.log(c);
+for (let b = 1; b <= 6; b++) {
+  let c = '';
+  let d = b % 2 === 1 ? 1 : 0;
+  
+  for (let e = 0; e < b; e++) {
+    c += ((d + e) % 2) + (e < b - 1 ? ' ' : '');
   }
+  console.log(c);
 }
-answer();
