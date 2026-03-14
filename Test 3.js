858157049aa6c6b6d516790cc532bc9c72d7f53f
Vansh Gosavi <129474804+VanshGosavi07@users.noreply.github.com>
2026-03-14 14:38:42 +0530

    Initial

---
+++ Test 3.js
@@ -0,0 +1,12 @@
+function answer(a = 6) {
+  for (let b = 1; b <= a; b++) {
+    let c = '';
+    let d = b % 2 === 1 ? 1 : 0;
+    
+    for (let e = 0; e < b; e++) {
+      c += ((d + e) % 2) + (e < b - 1 ? ' ' : '');
+    }
+    console.log(c);
+  }
+}
+answer();
